4de96c4af7f72b4fe83aac0320b03851ca80243f
TGab99 <torok.gabriella.990210@gmail.com>
2020-03-03 16:42:28 +0100

    Signup javítva

--- weblolalkalmazas/src/containers/Signup.js
+++ weblolalkalmazas/src/containers/Signup.js
@@ -1,122 +1,31 @@
-import React, { useState } from "react";
-import {
-  HelpBlock,
-  FormGroup,
-  FormControl,
-  ControlLabel
-} from "react-bootstrap";
-import LoaderButton from "../components/LoaderButton";
-import { useFormFields } from "../libs/hooksLib";
+import React, {Component} from "react";
 import "./Signup.css";
 
-export default function Signup(props) {
-  const [fields, handleFieldChange] = useFormFields({
-    email: "",
-    password: "",
-    confirmPassword: "",
-    confirmationCode: ""
-  });
-  const [newUser, setNewUser] = useState(null);
-  const [isLoading, setIsLoading] = useState(false);
-
-  function validateForm() {
+export default class Singup extends Component{
+  render(){
     return (
-      fields.email.length > 0 &&
-      fields.password.length > 0 &&
-      fields.password === fields.confirmPassword
-    );
-  }
+      <form>
+        <h3>Sign up</h3>
 
-  function validateConfirmationForm() {
-    return fields.confirmationCode.length > 0;
-  }
+        <div className="from-group">
+          <label>First name</label>
+          <input type="text" className="from-control" placeholder="First name"/>
 
-  async function handleSubmit(event) {
-    event.preventDefault();
+          <label>Last name</label>
+          <input type="text" className="from-control" placeholder="Last name"/>
 
-    setIsLoading(true);
+          <label>Username</label>
+          <input type="text" className="from-control" placeholder="Username"/>
 
-    setNewUser("test");
+          <label>Email address</label>
+          <input type="email" className="from-control" placeholder="Email"/>
 
-    setIsLoading(false);
-  }
+          <label>Password</label>
+          <input type="password" className="from-control" placeholder="Password"/>
 
-  async function handleConfirmationSubmit(event) {
-    event.preventDefault();
-
-    setIsLoading(true);
-  }
-
-  function renderConfirmationForm() {
-    return (
-      <form onSubmit={handleConfirmationSubmit}>
-        <FormGroup controlId="confirmationCode" bsSize="large">
-          <ControlLabel>Confirmation Code</ControlLabel>
-          <FormControl
-            autoFocus
-            type="tel"
-            onChange={handleFieldChange}
-            value={fields.confirmationCode}
-          />
-          <HelpBlock>Please check your email for the code.</HelpBlock>
-        </FormGroup>
-        <LoaderButton
-          block
-          type="submit"
-          bsSize="large"
-          isLoading={isLoading}
-          disabled={!validateConfirmationForm()}
-        >
-          Verify
-        </LoaderButton>
+          <button type="submit" className="btn btn-primary btn-block">Sign up</button>
+        </div>
       </form>
     );
   }
-
-  function renderForm() {
-    return (
-      <form onSubmit={handleSubmit}>
-        <FormGroup controlId="email" bsSize="large">
-          <ControlLabel>Email</ControlLabel>
-          <FormControl
-            autoFocus
-            type="email"
-            value={fields.email}
-            onChange={handleFieldChange}
-          />
-        </FormGroup>
-        <FormGroup controlId="password" bsSize="large">
-          <ControlLabel>Password</ControlLabel>
-          <FormControl
-            type="password"
-            value={fields.password}
-            onChange={handleFieldChange}
-          />
-        </FormGroup>
-        <FormGroup controlId="confirmPassword" bsSize="large">
-          <ControlLabel>Confirm Password</ControlLabel>
-          <FormControl
-            type="password"
-            onChange={handleFieldChange}
-            value={fields.confirmPassword}
-          />
-        </FormGroup>
-        <LoaderButton
-          block
-          type="submit"
-          bsSize="large"
-          isLoading={isLoading}
-          disabled={!validateForm()}
-        >
-          Signup
-        </LoaderButton>
-      </form>
-    );
-  }
-
-  return (
-    <div className="Signup">
-      {newUser === null ? renderForm() : renderConfirmationForm()}
-    </div>
-  );
 }
