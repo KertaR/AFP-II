5110802baff86e83e2633b12080d30476a9e534d
Kuborcik Ádám <adamome1337@gmail.com>
2020-04-15 19:59:45 +0200

    Osztályok áthelyezve java-n belülre

--- LOF/app/src/main/java/com/example/lof/MainActivity.java
+++ LOF/app/src/main/java/com/example/lof/MainActivity.java
@@ -12,8 +12,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Button Bejelentkezés = findViewById(R.id.button);
-        Bejelentkezés.setOnClickListener(new View.OnClickListener() {
+        Button Bejelentkezes = findViewById(R.id.button);
+        Bejelentkezes.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 setContentView(R.layout.activity_player_menu);
