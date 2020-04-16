4a32e418262554ac0fdc0d3a5bbea01f7029978b
Sakusa <SakusaUchiha@users.noreply.github.com>
2020-04-16 13:33:20 +0200

    Frontend kialakítása
    Ragisztrációs felület, Jogi nyilatkozat, Az alkotólról és a Felhasználási felületek kialakítása.

--- LOF/app/src/main/java/com/example/lof/MainActivity.java
+++ LOF/app/src/main/java/com/example/lof/MainActivity.java
@@ -6,6 +6,8 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
+import java.lang.annotation.Documented;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
@@ -17,6 +19,46 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 setContentView(R.layout.activity_player_menu);
+            }
+        });
+
+        Button Regisztráció = findViewById(R.id.button7);
+        Regisztráció.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                setContentView(R.layout.activity_registration);
+            }
+        });
+
+        Button Vissza = findViewById(R.id.button16);
+        Vissza.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                setContentView(R.layout.activity_main);
+            }
+        });
+
+        Button Felhasznalai_feltetelek = findViewById(R.id.button9);
+        Felhasznalai_feltetelek.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                setContentView(R.layout.activity_terms_of_use);
+            }
+        });
+
+        Button Az_alkotókról = findViewById(R.id.button10);
+        Az_alkotókról.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                setContentView(R.layout.activity_creator);
+            }
+        });
+
+        Button Jogi_nyilatkozat = findViewById(R.id.button11);
+        Jogi_nyilatkozat.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                setContentView(R.layout.activity_legal_notice);
             }
         });
     }
