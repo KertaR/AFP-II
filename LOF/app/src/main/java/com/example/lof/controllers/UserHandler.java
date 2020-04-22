62c9e9e2089701ab14572a6ecfea28b2ef0e921e
Kuborcik Ádám <adamome1337@gmail.com>
2020-04-22 13:28:09 +0200

    Session és Lekérdezések létrehozása
    Létrehoztam egy session mezőt, ami arra lesz jó hogy tudjuk hogy az adott felhasználó bevan e lépve. Ezt kell használni a lekérdezésekhez majd.
    Illetve létrehoztam pár lekérdezést.

--- LOF/app/src/main/java/com/example/lof/controllers/UserHandler.java
+++ LOF/app/src/main/java/com/example/lof/controllers/UserHandler.java
@@ -13,6 +13,25 @@ import com.example.lof.datastructures.User;
 public class UserHandler {
     private String DB_NAME = "sakoshi";
     private static AppDatabase database;
+    private String sessionid;
+    public String getSession(){
+        return sessionid;
+    }
+    private void setSession(String sessionid){
+        this.sessionid = sessionid;
+    }
+    /*
+        Ha bejelentkezik a felhasználó, akkor a 'sessionid'-t állítsuk be
+        a "setSession" metódus segítségével a bejelentkezett felhasználó nevével.
+
+        Amennyiben tartalmaz a 'sessionid' valamit, az azt jeleni hogy be van lépve.
+        Amennyiben nincs semmi, akkor nincs belépve, egy ellenőrzéssel meglehet mondani hogy az adott felhasználó
+        bevan lépve, és a nevével lekérdezéseket tudunk végrehajtani.
+
+        Amennyiben a felhasználó kijelentkezik, a sessionid-t ki kell törölni.
+
+
+     */
     public UserHandler(Context context) {
         database = Room.databaseBuilder(context, AppDatabase.class, DB_NAME).build();
     }
--- LOF/app/src/main/java/com/example/lof/database/UserDao.java
+++ LOF/app/src/main/java/com/example/lof/database/UserDao.java
@@ -3,9 +3,12 @@ package com.example.lof.database;
 import androidx.room.Dao;
 import androidx.room.Delete;
 import androidx.room.Insert;
+import androidx.room.Query;
 import androidx.room.Update;
 
 import com.example.lof.datastructures.User;
+
+import java.util.List;
 
 @Dao
 public interface UserDao {
@@ -15,4 +18,10 @@ public interface UserDao {
     public void updateUsers(User user);
     @Delete
     public void deleteUsers(User user);
+    @Query("SELECT * FROM users")
+    List<User> getAll();
+    @Query("SELECT username FROM users WHERE username = :username")
+    public String getUsername(String username);
+    @Query("SELECT * FROM users WHERE username = :username")
+    public User[] getUser(String username);
 }
