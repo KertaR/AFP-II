274e0594ccb1c231f5b43c0ce8c9d239c38191c7
Járomi Dávid Antal <43856444+jaromi98@users.noreply.github.com>
2020-04-25 16:54:18 +0200

    Shizuki karakter képességei megírva
    Shizuki mind a 4 képessége megírva

--- LOF/app/src/main/java/com/example/lof/datastructures/Champions/Shizuki.java
+++ LOF/app/src/main/java/com/example/lof/datastructures/Champions/Shizuki.java
@@ -10,21 +10,59 @@ public class Shizuki extends Character implements Skill{
 
     @Override
     public void PrimarySkill(Character own, Character enemy) {
-
+        double SMana = own.getMana();
+        if(SMana >= 50){
+            SMana -= 50;
+            own.setMana(SMana);
+            double EHealth = enemy.getHealthpoints();
+            if(EHealth > 80){
+                EHealth -= 80;
+                enemy.setHealthpoints(EHealth);
+            }
+            else{
+                // az ellenfél meghal
+            }
+        }
     }
 
     @Override
     public void SecondarySkill(Character own, Character enemy) {
-
+        double SMana = own.getMana();
+        if(SMana >= 80){
+            SMana += 35;
+            own.setMana(SMana);
+            double SManaregen = own.getManaregen();
+            SManaregen += 5;
+            own.setManaregen(SManaregen);
+        }
     }
 
     @Override
     public void TertiarySkill(Character own, Character enemy) {
-
+        double SMana = own.getMana();
+        if(SMana >= 170){
+            SMana -= 170;
+            own.setMana(SMana);
+            double EHealth = enemy.getHealthpoints();
+            if(EHealth > 155){
+                EHealth -= 155;
+                enemy.setHealthpoints(EHealth);
+            }
+            else{
+                // az ellenfél meghal
+            }
+        }
     }
 
     @Override
     public void QuaternarySkill(Character own, Character enemy) {
-
+        double SMana = own.getMana();
+        if(SMana >= 65){
+            SMana -= 65;
+            own.setMana(SMana);
+            double SDefence = own.getDefence();
+            SDefence += 35;
+            own.setDefence(SDefence);
+        }
     }
 }
