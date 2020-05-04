7a38451714b38ebe280108acf44d27ee10e4a9a5
Kuborcik Ádám <adamome1337@gmail.com>
2020-05-04 17:19:07 +0200

    Gombok felvitele a felületre
    Felraktam a gombokat, hogy hozzátudjam kötni a metódusokat.

--- LOF/app/src/main/java/com/example/lof/KarakterValasztasActivity.java
+++ LOF/app/src/main/java/com/example/lof/KarakterValasztasActivity.java
@@ -3,10 +3,16 @@ package com.example.lof;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ImageButton;
 import android.widget.TableLayout;
 import com.example.lof.views.KarakterValasztView;
 
 public class KarakterValasztasActivity extends AppCompatActivity {
+
+    ImageButton btn_yoshi = findViewById(R.id.btn_yoshi);
+    ImageButton btn_ameyuki = findViewById(R.id.btn_yoshi);
+    ImageButton btn_yoshi = findViewById(R.id.btn_yoshi);
+    ImageButton btn_yoshi = findViewById(R.id.btn_yoshi);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
