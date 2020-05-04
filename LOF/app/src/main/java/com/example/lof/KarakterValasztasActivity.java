52f8e9e4a4e247a178fe193da00df90b4596532d
Kuborcik Ádám <adamome1337@gmail.com>
2020-05-04 17:37:10 +0200

    Karakterválasztás eventek létrehozása.

--- LOF/app/src/main/java/com/example/lof/KarakterValasztasActivity.java
+++ LOF/app/src/main/java/com/example/lof/KarakterValasztasActivity.java
@@ -2,17 +2,26 @@ package com.example.lof;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.TableLayout;
+
+import com.example.lof.datastructures.Champions.Ameyuki_hercegno;
+import com.example.lof.datastructures.Champions.Sakusa;
+import com.example.lof.datastructures.Champions.Shizuki;
+import com.example.lof.datastructures.Champions.Yoshi;
+import com.example.lof.datastructures.Character;
 import com.example.lof.views.KarakterValasztView;
 
 public class KarakterValasztasActivity extends AppCompatActivity {
 
     ImageButton btn_yoshi = findViewById(R.id.btn_yoshi);
-    ImageButton btn_ameyuki = findViewById(R.id.btn_yoshi);
-    ImageButton btn_yoshi = findViewById(R.id.btn_yoshi);
-    ImageButton btn_yoshi = findViewById(R.id.btn_yoshi);
+    ImageButton btn_ameyuki = findViewById(R.id.btn_ameyuki);
+    ImageButton btn_shizuki = findViewById(R.id.btn_shizuki);
+    ImageButton btn_sakusa = findViewById(R.id.btn_sakusa);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +29,31 @@ public class KarakterValasztasActivity extends AppCompatActivity {
         setContentView(R.layout.activity_karakter_valasztas);
         TableLayout table = findViewById(R.id.karaktertable);
         KarakterValasztView.TablaFeltolt(table);
+        btn_yoshi.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View registration) {
+                playYoshi();
+            }
+        });
+    }
+    public void playYoshi(){
+        Character character = new Yoshi();
+        Intent game = new Intent(this,GameActivity.class);
+        startActivity(game);
+    }
+    public void playAmeyuki(){
+        Character character = new Ameyuki_hercegno();
+        Intent game = new Intent(this,GameActivity.class);
+        startActivity(game);
+    }
+    public void playShizuki(){
+        Character character = new Shizuki();
+        Intent game = new Intent(this,GameActivity.class);
+        startActivity(game);
+    }
+    public void playSakusa(){
+        Character character = new Sakusa();
+        Intent game = new Intent(this,GameActivity.class);
+        startActivity(game);
     }
 }
