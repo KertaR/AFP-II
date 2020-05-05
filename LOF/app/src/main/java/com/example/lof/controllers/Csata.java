c04eda632cc04ad1291c45c2e86bf884bc9f3a01
Kuborcik Ádám <adamome1337@gmail.com>
2020-05-05 10:42:27 +0200

    Csata osztály mentés

--- LOF/app/src/main/java/com/example/lof/controllers/Csata.java
+++ LOF/app/src/main/java/com/example/lof/controllers/Csata.java
@@ -25,6 +25,7 @@ public class Csata {
     public static void KorCsata(Character enemy, Character player){
         if(!tevagy){
             enemy.Bot(player,enemy);
+            tevagy = true;
         }
     }
 }
