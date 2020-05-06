38e974d1df219f7a6c29fda081a014ba7ec4d1e0
Sakusa <SakusaUchiha@users.noreply.github.com>
2020-05-06 12:40:00 +0200

    Karakterválasztási felület befejezése
    Karakterválasztási felülethez hozzáaadjuk a karakterek leírását.

--- LOF/app/src/main/java/com/example/lof/KarakterValasztasActivity.java
+++ LOF/app/src/main/java/com/example/lof/KarakterValasztasActivity.java
@@ -7,6 +7,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
+import android.widget.ImageView;
 import android.widget.TableLayout;
 
 import com.example.lof.datastructures.Champions.Ameyuki_hercegno;
@@ -35,24 +36,32 @@ public class KarakterValasztasActivity extends AppCompatActivity {
             @Override
             public void onClick(View registration) {
                 kivalasztottkarakter = "Yoshi";
+                ImageView img = (ImageView) findViewById(R.id.karaktervalasztas);
+                img.setImageResource(R.drawable.leiras_yoshi);
             }
         });
         btn_ameyuki.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View registration) {
                 kivalasztottkarakter = "Ameyuki";
+                ImageView img = (ImageView) findViewById(R.id.karaktervalasztas);
+                img.setImageResource(R.drawable.leiras_ameyuki);
             }
         });
         btn_shizuki.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View registration) {
                 kivalasztottkarakter = "Shizuki";
+                ImageView img = (ImageView) findViewById(R.id.karaktervalasztas);
+                img.setImageResource(R.drawable.leiras_shizuki);
             }
         });
         btn_sakusa.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View registration) {
                 kivalasztottkarakter = "Sakusa";
+                ImageView img = (ImageView) findViewById(R.id.karaktervalasztas);
+                img.setImageResource(R.drawable.leiras_sakusa);
             }
         });
         btn_meccsinditas.setOnClickListener(new View.OnClickListener() {
