b3860bc32b1c7593e4ac20b5f57f84615d122031
Kuborcik Ádám <adamome1337@gmail.com>
2020-05-06 14:26:48 +0200

    Karakterválasztás felület javítva
    Nem lehetett a karakterválasztás felületre átmenni, a gombok inicializálását a contentview alá írtam.

--- LOF/app/src/main/java/com/example/lof/KarakterValasztasActivity.java
+++ LOF/app/src/main/java/com/example/lof/KarakterValasztasActivity.java
@@ -4,7 +4,9 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.ImageView;
@@ -20,21 +22,19 @@ import com.example.lof.views.KarakterValasztView;
 import java.util.List;
 
 public class KarakterValasztasActivity extends AppCompatActivity {
-
-    ImageButton btn_yoshi = findViewById(R.id.btn_yoshi);
-    ImageButton btn_ameyuki = findViewById(R.id.btn_ameyuki);
-    ImageButton btn_shizuki = findViewById(R.id.btn_shizuki);
-    ImageButton btn_sakusa = findViewById(R.id.btn_sakusa);
-    Button btn_meccsinditas = findViewById(R.id.meccsinditas);
     String kivalasztottkarakter;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_karakter_valasztas);
+        ImageButton btn_yoshi = (ImageButton)this.findViewById(R.id.btn_yoshi);
+        ImageButton btn_ameyuki = (ImageButton)this.findViewById(R.id.btn_ameyuki);
+        ImageButton btn_shizuki = (ImageButton)this.findViewById(R.id.btn_shizuki);
+        ImageButton btn_sakusa = (ImageButton)this.findViewById(R.id.btn_sakusa);
+        Button btn_meccsinditas = (Button)this.findViewById(R.id.meccsinditas);
         btn_yoshi.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View registration) {
+            public void onClick(View yoshi) {
                 kivalasztottkarakter = "Yoshi";
                 ImageView img = (ImageView) findViewById(R.id.karaktervalasztas);
                 img.setImageResource(R.drawable.leiras_yoshi);
@@ -42,7 +42,7 @@ public class KarakterValasztasActivity extends AppCompatActivity {
         });
         btn_ameyuki.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View registration) {
+            public void onClick(View ameyuki) {
                 kivalasztottkarakter = "Ameyuki";
                 ImageView img = (ImageView) findViewById(R.id.karaktervalasztas);
                 img.setImageResource(R.drawable.leiras_ameyuki);
@@ -50,7 +50,7 @@ public class KarakterValasztasActivity extends AppCompatActivity {
         });
         btn_shizuki.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View registration) {
+            public void onClick(View shizuki) {
                 kivalasztottkarakter = "Shizuki";
                 ImageView img = (ImageView) findViewById(R.id.karaktervalasztas);
                 img.setImageResource(R.drawable.leiras_shizuki);
@@ -58,7 +58,7 @@ public class KarakterValasztasActivity extends AppCompatActivity {
         });
         btn_sakusa.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View registration) {
+            public void onClick(View sakusa) {
                 kivalasztottkarakter = "Sakusa";
                 ImageView img = (ImageView) findViewById(R.id.karaktervalasztas);
                 img.setImageResource(R.drawable.leiras_sakusa);
