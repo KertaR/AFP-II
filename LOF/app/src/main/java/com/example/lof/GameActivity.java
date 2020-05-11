910c6c1380dd55257711d3fc523ec9efa15e2c4a
Kuborcik Ádám <adamome1337@gmail.com>
2020-05-11 11:18:22 +0200

    Játék elkészítése
    Hozzárendeltem a TextViewhez és ImageViewhez a változókat a legenerált karakter példányokból.

--- LOF/app/src/main/java/com/example/lof/GameActivity.java
+++ LOF/app/src/main/java/com/example/lof/GameActivity.java
@@ -2,12 +2,15 @@ package com.example.lof;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.media.Image;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.example.lof.controllers.Csata;
 import com.example.lof.datastructures.Champions.Ameyuki_hercegno;
@@ -21,11 +24,30 @@ import java.util.Random;
 public class GameActivity extends AppCompatActivity {
     Character player,enemy;
     boolean kiaz;
+    int gold;
+    double health;
+    double mana;
+    double manaenemy;
+    double healthenemy;
+    TextView enemyhealth;
+    TextView enemymana;
+    ImageView enemypicture;
+    TextView selfhealth;
+    TextView selfmana;
+    TextView selfgold;
+    ImageView selfpicture;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.game);
         Intent intent = getIntent();
+        enemyhealth = findViewById(R.id.enemyhealth);
+        enemymana = findViewById(R.id.enemymana);
+        enemypicture = findViewById(R.id.enemypicture);
+        selfhealth = findViewById(R.id.selfhealth);
+        selfmana = findViewById(R.id.selfmana);
+        selfgold = findViewById(R.id.selfgold);
+        selfpicture = findViewById(R.id.selfpicture);
         boolean korvege = false;
         String karakternev = intent.getStringExtra("karakter");
         kiaz = Csata.Sorsol();
@@ -39,34 +61,55 @@ public class GameActivity extends AppCompatActivity {
             @Override
             public void onClick(View attack) {
                 player.Attack(player,enemy);
+                enemy.Bot(enemy,player);
+                player.EndRound();
+                enemy.EndRound();
+                CurrentStatus();
             }
         });
         skill1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View skill1) {
                 player.PrimarySkill(enemy);
+                enemy.Bot(enemy,player);
+                player.EndRound();
+                enemy.EndRound();
+                CurrentStatus();
             }
         });
         skill2.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View skill2) {
                 player.SecondarySkill(enemy);
+                enemy.Bot(enemy,player);
+                player.EndRound();
+                enemy.EndRound();
+                CurrentStatus();
             }
         });
         skill3.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View skill3) {
                 player.TertiarySkill(enemy);
+                enemy.Bot(enemy,player);
+                player.EndRound();
+                enemy.EndRound();
+                CurrentStatus();
             }
         });
         skill4.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View skill4) {
                 player.QuaternarySkill(enemy);
+                enemy.Bot(enemy,player);
+                player.EndRound();
+                enemy.EndRound();
+                CurrentStatus();
             }
         });
 
     }
+    @SuppressLint("ResourceType")
     private void JatekInicalizalasa(String karakternev){
         Character character = null;
         switch (karakternev){
@@ -74,33 +117,58 @@ public class GameActivity extends AppCompatActivity {
             {
                 character = null;
                 character = new Ameyuki_hercegno();
+                selfpicture.setImageResource(R.drawable.ameyuki_profil);
             }
             break;
             case "Yoshi":
             {
                 character = null;
                 character = new Yoshi();
+                selfpicture.setImageResource(R.drawable.yoshi_profil);
             }
             break;
             case "Sakusa":
             {
                 character = null;
                 character = new Sakusa();
+                selfpicture.setImageResource(R.drawable.sakusa);
             }
             break;
             case "Shizuki": {
                 character = null;
                 character = new Shizuki();
+                selfpicture.setImageResource(R.drawable.shizuki_profil);
             }
             break;
         }
         player = character;
+        gold=100;
         Random rnd = new Random();
         int random = rnd.nextInt(100);
-        if(random >= 0 && random <= 25) enemy = new Ameyuki_hercegno();
-        else if(random > 25 && random <= 50 ) enemy = new Yoshi();
-        else if(random > 50 && random <= 75 ) enemy = new Sakusa();
-        else if(random > 75 && random <= 100 ) enemy = new Shizuki();
-
+        if(random >= 0 && random <= 25){
+            enemy = new Ameyuki_hercegno();
+            enemypicture.setImageResource(R.drawable.ameyuki_profil);
+        }
+        else if(random > 25 && random <= 50 ){
+            enemy = new Yoshi();
+            enemypicture.setImageResource(R.drawable.yoshi_profil);
+        }
+        else if(random > 50 && random <= 75 ){
+            enemy = new Sakusa();
+            enemypicture.setImageResource(R.drawable.sakusa);
+        }
+        else if(random > 75 && random <= 100 ){
+            enemy = new Shizuki();
+            enemypicture.setImageResource(R.drawable.shizuki_profil);
+        }
+        CurrentStatus();
+    }
+    public void CurrentStatus()
+    {
+        selfgold.setText(String.valueOf(gold));
+        selfhealth.setText(String.valueOf((int)player.getHealthpoints()));
+        selfmana.setText(String.valueOf((int)player.getMana()));
+        enemyhealth.setText(String.valueOf((int)enemy.getHealthpoints()));
+        enemymana.setText(String.valueOf((int)enemy.getMana()));
     }
 }
