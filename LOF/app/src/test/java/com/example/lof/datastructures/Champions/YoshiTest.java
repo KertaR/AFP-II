aa4533a2ba785a96663df4a71eb2525fdb0cc2ec
TGab99 <55789282+TGab99@users.noreply.github.com>
2020-05-15 17:40:21 +0200

    Unit-teszt módosítva: QuaternarySkill metódus tesztelve

--- LOF/app/src/test/java/com/example/lof/datastructures/Champions/YoshiTest.java
+++ LOF/app/src/test/java/com/example/lof/datastructures/Champions/YoshiTest.java
@@ -7,23 +7,34 @@ import static org.junit.Assert.*;
 
 public class YoshiTest {
     Yoshi y = new Yoshi();
-    Yoshi y2 = new Yoshi();
     Sakusa s = new Sakusa();
-    Sakusa s2 = new Sakusa();
 
     double mana = 225;
-    double eHealth = 380;
+    double yHealth = 680;
+    double sHealth = 380;
 
     @Test
     public void tertiarySkill() {
         if(mana >= 70){
             mana -= 70;
-            y2.setMana(mana);
-            eHealth -= 115;
-            s2.setHealthpoints(eHealth);
+            sHealth -= 115;
         }
 
         y.TertiarySkill(s);
-        assert(mana == y.getMana() && eHealth == s.getHealthpoints());
+        assert(mana == y.getMana() && sHealth == s.getHealthpoints());
+    }
+
+    @Test
+    public void quaternarySill(){
+        if(mana >= 40){
+            mana -= 40;
+            if((yHealth + 90) <= 680){
+                yHealth += 90;
+            }else{
+                yHealth = 680;
+            }
+        }
+        y.QuaternarySkill(s);
+        assert(mana == y.getMana() && yHealth == y.getHealthpoints());
     }
 }
