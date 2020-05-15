714b74bc1b7bb2b84fdb75d0c39d340cd0db1bbd
TGab99 <55789282+TGab99@users.noreply.github.com>
2020-05-15 17:31:51 +0200

    Unit-teszt módosítva: tertiarySill metódus tesztelve

--- LOF/app/src/test/java/com/example/lof/datastructures/Champions/Ameyuki_hercegnoTest.java
+++ LOF/app/src/test/java/com/example/lof/datastructures/Champions/Ameyuki_hercegnoTest.java
@@ -9,33 +9,36 @@ public class Ameyuki_hercegnoTest {
     Ameyuki_hercegno ah = new Ameyuki_hercegno();
     Yoshi y = new Yoshi();
 
-    double mana;
-    double sAttackDamage;
-    double eAttackDamage;
-    double eDefence;
-    double eHealth;
+    double mana = 240;
+    double sAttackDamage = 40;
+    double eAttackDamage = 55;
+    double eDefence = 1.8;
+    double eHealth = 680;
     double oAttackDamage;
-
-    @Before
-    public void set(){
-        mana = ah.getMana();
-        sAttackDamage = ah.getAttackdamage();
-        eAttackDamage = y.getAttackdamage();
-        eDefence = y.getDefence();
-        eHealth = y.getHealthpoints();
-    }
 
     @Test
     public void quaternarySkill() {
         if(mana >= 180){
             mana -= 180;
-            ah.setMana(mana);
             oAttackDamage = (sAttackDamage + eAttackDamage + 100)/eDefence;
             eHealth -= oAttackDamage;
-            y.setHealthpoints(eHealth);
         }
 
         ah.QuaternarySkill(y);
         assert(mana == ah.getMana() && eHealth == y.getHealthpoints());
+    }
+
+    @Test
+    public void tertiarySkill(){
+        if(mana >= 100){
+            if((mana + 20) <= 240){
+                mana += 20;
+            }else{
+                mana = 240;
+            }
+        }
+
+        ah.TertiarySkill(y);
+        assert(mana == ah.getMana());
     }
 }
