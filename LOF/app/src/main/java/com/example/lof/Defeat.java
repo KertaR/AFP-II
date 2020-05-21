831a11f8de7825b4df3706141f30056bfef19a7b
TGab99 <55789282+TGab99@users.noreply.github.com>
2020-05-21 14:03:02 +0200

    setXP metódus megírva.

--- LOF/app/src/main/java/com/example/lof/Defeat.java
+++ LOF/app/src/main/java/com/example/lof/Defeat.java
@@ -4,10 +4,13 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class Defeat extends AppCompatActivity {
+    TextView XP = findViewById(R.id.textView49);
+
     @Override
     protected void onCreate(Bundle saveInstanceState){
         super.onCreate(saveInstanceState);
@@ -24,5 +27,9 @@ public class Defeat extends AppCompatActivity {
     public void openPlayerMenu(){
         Intent player_menu = new Intent(this, PlayerMenu.class);
         startActivity(player_menu);
+    }
+
+    public void setXP(int xp){
+        XP.setText("Kapott XP: " + String.valueOf((xp)));
     }
 }
